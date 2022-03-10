9fab94bd4cf42be18a997e0ac938cab241725278
koval <88kg@mail.ru>
2022-03-10 16:54:39 +0300

    hello

---
+++ README.md
@@ -0,0 +1,3 @@
+## Compile 
+
+GOOS=linux GOARCH=amd64 go build -o ./notifications -a
--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-//GOOS=linux GOARCH=amd64 go build -o ./notifications -a
-
 import (
 	"context"
 	"encoding/json"
